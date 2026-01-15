c3b30c22cf883c057aacea56d83caf825a54c5dc
Nickzanarak <partarwee@gmail.com>
2026-01-15 11:22:06 +0700

    update edittt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Content, Footer, Header } from "./func-components";
 import RefArray from "./refs-array";
+import MessageBox from "./state-func";
 
 
 
 function App() {
-  return <RefArray/>
+  return <MessageBox/>
 }
 
 export default App;
