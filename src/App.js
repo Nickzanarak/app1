3f44062aa6fbfff98c7936051ce08ea3055fd0d8
Nickzanarak <partarwee@gmail.com>
2026-01-15 10:38:31 +0700

    RefArray

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Content, Footer, Header } from "./func-components";
-import Calendar from "./class-components";
-import Button from "./class-components";
-import { Calculator2 } from "./calculator";
-import { EventData2 } from "./event-data";
+import RefArray from "./refs-array";
+
 
 
 function App() {
-  return <EventData2/>
+  return <RefArray/>
 }
 
 export default App;
