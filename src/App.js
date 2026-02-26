97fc08dfef7cebddcb0af0051039293c242d31c9
Nickzanarak <partarwee@gmail.com>
2026-02-26 10:54:02 +0700

    Form

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Content2 from "./context-content2";
 import Router2 from "./router2";
 import Router3 from "./router3";
 import FormText from "./form-text";
+import FormSelectFile from "./form-select-file";
 
 function App() {
   return (
-    <FormText/>
+    <FormSelectFile/>
   )
 }
 
